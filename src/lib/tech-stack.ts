848ac58ae84ed1b5ddf47175e87ee037a8259d69
w4keupvan <203548283+w4keupvan@users.noreply.github.com>
2025-03-25 21:37:15 +0800

    feat: add tech stack

---
+++ src/lib/tech-stack.ts
@@ -0,0 +1,84 @@
+const techStack: Array<{ src: string; alt: string }> = [
+  {
+    src: "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/HTML.svg",
+    alt: "HTML",
+  },
+  {
+    src: "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/CSS.svg",
+    alt: "CSS",
+  },
+  {
+    src: "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/TailwindCSS-Dark.svg",
+    alt: "Tailwind",
+  },
+  {
+    src: "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/JavaScript.svg",
+    alt: "JavaScript",
+  },
+  {
+    src: "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/TypeScript.svg",
+    alt: "TypeScript",
+  },
+  {
+    src: "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/React-Dark.svg",
+    alt: "React",
+  },
+  {
+    src: "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/NextJS-Dark.svg",
+    alt: "NextJS",
+  },
+  {
+    src: "https://raw.githubusercontent.com/tandpfun/skill-icons/refs/heads/main/icons/Svelte.svg",
+    alt: "Svelte",
+  },
+  {
+    src: "https://raw.githubusercontent.com/tandpfun/skill-icons/refs/heads/main/icons/GoLang.svg",
+    alt: "Golang",
+  },
+  {
+    src: "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/NodeJS-Dark.svg",
+    alt: "Node JS",
+  },
+  {
+    src: "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/ExpressJS-Dark.svg",
+    alt: "Express JS",
+  },
+  {
+    src: "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/PostgreSQL-Dark.svg",
+    alt: "PostgreSQL",
+  },
+  {
+    src: "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/SQLite.svg",
+    alt: "SQLite",
+  },
+  {
+    src: "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Prisma.svg",
+    alt: "Prisma",
+  },
+  {
+    src: "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Pnpm-Dark.svg",
+    alt: "PNPM",
+  },
+  {
+    src: "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Git.svg",
+    alt: "Git",
+  },
+  {
+    src: "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Docker.svg",
+    alt: "Docker",
+  },
+  {
+    src: "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Bash-Dark.svg",
+    alt: "Bash",
+  },
+  {
+    src: "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Postman.svg",
+    alt: "Postman",
+  },
+  {
+    src: "https://raw.githubusercontent.com/tandpfun/skill-icons/65dea6c4eaca7da319e552c09f4cf5a9a8dab2c8/icons/Arch-Dark.svg",
+    alt: "Arch",
+  },
+];
+
+export default techStack;
