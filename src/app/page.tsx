83951ccdd4af425f31025275d7ada6332b149588
soapermanwun <203548283+soapermanwun@users.noreply.github.com>
2025-04-14 23:06:02 +0800

    feat: remove linkedin and github at home page to avoid redundancy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
-import { Mail, Github, Linkedin } from "lucide-react";
+import { Mail, Coffee } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <section className="h-screen flex flex-col justify-center items-center px-4">
+    <section className="h-full flex flex-col justify-center items-center px-4">
       <div className="text-center max-w-3xl space-y-4">
         <p className="text-xs text-muted-foreground">
           $ cat ~/home/ivan/me.txt
@@ -20,38 +21,18 @@ export default function Home() {
       </div>
 
       <div className="pt-8 flex gap-3 flex-wrap">
-        <a
-          href="https://github.com/soapermanwun"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button
-            variant="outline"
-            className="text-xs md:text-sm flex items-center gap-2"
-          >
-            <Github size={16} />
-            ./github.sh
+        <a href="https://buymeacoffee.com/justineivat" target="_blank">
+          <Button className="text-xs md:text-sm flex items-center gap-2">
+            <Coffee size={16} />
+            ./buy-me-a-coffee.sh
           </Button>
         </a>
-        <a
-          href="https://www.linkedin.com/in/justineivangueco/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button
-            variant="outline"
-            className="text-xs md:text-sm flex items-center gap-2"
-          >
-            <Linkedin size={16} />
-            ./linkedin.sh
-          </Button>
-        </a>
-        <a href="/contact">
+        <Link href="/contact">
           <Button className="text-xs md:text-sm flex items-center gap-2">
             <Mail size={16} />
             ./contact.sh
           </Button>
-        </a>
+        </Link>
       </div>
     </section>
   );
